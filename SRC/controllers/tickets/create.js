111d1b96c75114ee91c9a2aaa63dd885865eb4c4
Lucas <lucasluvn@gmail.com>
2026-07-17 17:55:32 -0300

    Criando o banco de dados

--- SRC/controllers/tickets/create.js
+++ SRC/controllers/tickets/create.js
@@ -1,4 +1,19 @@
+import {randomUUID} from "node:crypto"
+
+
 // colacando os parametros entre parentese eu posso passar los em qualquer ordem 
 export function create({request,response}) {
- return response.end("Teste ok!")
+  const {equipment,description, user_name,
+  } = request.body
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+}
+ return response.end(JSON.stringify(ticket))
 }
